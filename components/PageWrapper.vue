681d9986e58e6d75c33ea37471de3bc238d61fb0
Hongbusi <q951686662@qq.com>
2022-05-17 23:23:09 +0800

    feat: add page wrapper (#8)

---
+++ components/PageWrapper.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+  subtitle: string
+  description: string
+  btnLink?: string
+  btnText?: string
+  githubLink?: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="flex justify-between items-center mt-32px">
+    <div class="flex items-end">
+      <h1 class="mr-8px text-3xl">
+        {{ title }}
+      </h1>
+      <span class="opacity-70">{{ subtitle }}</span>
+    </div>
+    <div v-if="githubLink || btnLink" class="flex items-center">
+      <a v-if="btnLink" class="mr-12px btn-primary-small" :href="btnLink" target="_blank">
+        {{ btnText }}
+      </a>
+      <a v-if="githubLink" class="icon-primary i-carbon-logo-github" :href="githubLink" target="_blank" />
+    </div>
+  </div>
+  <p class="mt-6px opacity-70">
+    {{ description }}
+  </p>
+  <div class="mt-24px">
+    <slot />
+  </div>
+</template>
--- unocss.config.ts
+++ unocss.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   shortcuts: [
     ['bg-primary', 'bg-gray-200/70 dark:bg-gray-200/20 rounded'],
     ['btn-primary', 'inline-flex justify-center px-4 py-2 bg-primary'],
+    ['btn-primary-small', 'inline-flex justify-center px-2 py-1 bg-primary text-sm'],
     ['icon-primary', 'inline-block cursor-pointer select-none opacity-70 transition duration-200 ease-in-out hover:opacity-100']
   ],
   presets: [
