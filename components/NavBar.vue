b31fa982f4347bbed087a50d550a3391aebcd3cb
tick <66043405+TickHeart@users.noreply.github.com>
2022-05-17 14:15:11 +0800

    feat: init nav icon (#2)
    * feat: init nav icon
    
    * feat: 完成了nav的动画小球
    
    * fix: 修改了使用icon方式不当
    
    * fix: 修复了屏幕大小变化时小球的位置错位的问题
    
    * fix: 修改依赖引入的问题

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,19 +1,75 @@
+<script lang="ts" setup>
+import animejs from 'animejs'
+
+interface EleInfo {
+  mid: number
+  el: Element
+  key: string
+}
+
+const navRef = ref<HTMLElement | null>(null)
+const boxRef = ref<HTMLElement | null>(null)
+const eleInfo = ref<EleInfo[]>([])
+const route = useRoute()
+
+function collectEleInfo() {
+  eleInfo.value.length = 0
+  const { children } = navRef.value
+  for (let i = 0; i < children.length; i++) {
+    const child = children[i]
+    const { height, top } = child.getBoundingClientRect()
+    eleInfo.value.push({
+      mid: top + height / 2,
+      el: child,
+      key: child.getAttribute('data-key')
+    })
+  }
+}
+function dep() {
+  watch(() => route.name, (val) => {
+    boxToElePosition(val as string)
+  }, { immediate: true })
+}
+function boxToElePosition(name: string) {
+  const info = eleInfo.value.find(it => it.key === name)
+  const { height } = boxRef.value.getBoundingClientRect()
+  animejs({
+    targets: boxRef.value,
+    top: info.mid - height / 2
+  })
+}
+
+function resize() {
+  collectEleInfo()
+  boxToElePosition(route.name as string)
+}
+onMounted(() => {
+  collectEleInfo()
+  dep()
+  window.addEventListener('resize', resize)
+})
+onUnmounted(() => {
+  window.removeEventListener('resize', resize)
+})
+</script>
+
 <template>
-  <div class="sticky top-0 w-80px h-screen border-r border-gray-200">
-    <nuxt-link to="/">
-      Home
+  <div ref="navRef" class="sticky top-0 w-80px h-screen border-r border-gray-200 flex flex-col justify-center items-end pr-2 gap-8">
+    <nuxt-link to="/" data-key="index">
+      <div class="i-ri-home-smile-line text-xl" />
     </nuxt-link>
-    <nuxt-link to="/plans">
-      Plans
+    <nuxt-link to="/plans" data-key="plans">
+      <div class="i-ri-todo-line text-xl" />
     </nuxt-link>
-    <nuxt-link to="/weekly">
-      Weekly
+    <nuxt-link to="/weekly" data-key="weekly">
+      <div class="i-ic-baseline-menu-book text-xl" />
     </nuxt-link>
-    <nuxt-link to="/bookmarks">
-      Bookmarks
+    <nuxt-link to="/bookmarks" data-key="bookmarks">
+      <div class="i-ic-round-bookmarks text-xl" />
     </nuxt-link>
-    <nuxt-link to="/about">
-      About
+    <nuxt-link to="/about" data-key="about">
+      <div class="i-ri-team-line text-xl" />
     </nuxt-link>
+    <div ref="boxRef" class="w-2 h-2 bg-light-900 rounded-full absolute top-2 right-[-0.3rem]" />
   </div>
 </template>
