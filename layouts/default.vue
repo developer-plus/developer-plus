c042c72a8034a03679a2c29cd2fa92904c8dc675
Hongbusi <q951686662@qq.com>
2022-05-17 09:28:45 +0800

    feat: layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,18 @@
 <template>
-  <main class="py-20 px-10 text-center">
-    <slot />
-    <div class="mt-5 mx-auto text-center opacity-25 text-sm">
-      [Default Layout]
+  <main class="flex px-10 mx-auto w-1500px">
+    <div class="sticky top-0 w-80px h-screen border-r border-gray-200 bg-green">
+      nav
     </div>
 
-    <dark-toggle />
+    <div class="flex-1 flex justify-center bg-red">
+      <div class="mx-32px max-w-700px bg-white">
+        <slot />
+      </div>
+    </div>
+
+    <div class="px-32px w-380px min-h-screen border-l border-gray-200 bg-yellow">
+      developer-plus
+      <dark-toggle />
+    </div>
   </main>
 </template>
