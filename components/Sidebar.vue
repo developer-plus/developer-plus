f06e3e29e9267e9ab48d137616935dfa93f277b7
hk.xia <595424932@qq.com>
2022-05-17 15:37:07 +0800

    style: fix icon position

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="px-32px w-380px min-h-screen border-l border-gray-200">
-    developer-plus
-    <dark-toggle />
+    <div flex gap-3>
+      developer-plus
+      <dark-toggle />
+    </div>
   </div>
 </template>
