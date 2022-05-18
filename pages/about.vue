ee2450e826783a8e16f26d2143e4c2cb3e8c4340
Hongbusi <q951686662@qq.com>
2022-05-18 23:26:38 +0800

    chore: update why

--- pages/about.vue
+++ pages/about.vue
@@ -14,15 +14,11 @@ const members = await $fetch<ITeamMember[]>('/api/team-members')
 <template>
   <page-wrapper v-bind="options">
     <page-module title="为什么叫 developer-plus？">
-      <div class="opacity-70 indent-2em">
-        在决定去创建组织之后，和几个群友讨论了一下关于组织名称的问题，得到了很多答案：hbs-admin、hbs-design、hbs-team...，但是好像和我的想法并不吻合。
-
-        想要创建这个组织，或者说想要去做 vue-hbs-admin 这个开源项目最根本的原因，是因为想要找一群小伙伴共同学习，共同进步，可以互相 review 代码，享受开源的快乐。我想要的是一个团队，而不是带着 hbs
-        这种个人色彩的名称。
-
-        纠结了很久，也想到了很多满意的名字，如：developers...（糟糕，其他的想不起了...）。但是这些名字都被抢注了，很失望。
-
-        不知道怎么滴，突然爆出了 plus 这个单词，可能是受 element-plus 的影响，也有可能是因为我已经想好了 logo 的设计。于是乎，名字敲定，第一个组织成功创建，开心地把 vue-hbs-admin 迁移到组织。
+      <div class="opacity-70">
+        开发者之上：来源于开发者，服务于开发者。
+        <a class="ml-12px btn-primary-small" href="https://github.com/developer-plus/guide/wiki/%E4%B8%BA%E4%BB%80%E4%B9%88%E5%8F%AB-developer-plus%EF%BC%9F" target="_blank">
+          详细介绍
+        </a>
       </div>
     </page-module>
 
