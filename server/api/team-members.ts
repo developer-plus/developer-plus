533d9e652b2b58f8f498c65897b59ad05a8ee31e
Hongbusi <q951686662@qq.com>
2022-05-19 20:27:27 +0800

    feat: team member

--- server/api/team-members.ts
+++ server/api/team-members.ts
@@ -13,14 +13,14 @@ const teamMembers: ITeamMember[] = [
   {
     name: 'TickHeart',
     avatar: 'https://avatars.githubusercontent.com/u/66043405?v=4',
-    tag: 'Member',
+    tag: 'Core',
     address: 'Hebei, China',
     githubLink: 'https://github.com/TickHeart'
   },
   {
     name: 'alexzhang1030',
     avatar: 'https://avatars.githubusercontent.com/u/49969959?v=4',
-    tag: 'Member',
+    tag: 'Core',
     address: 'Beijing, China',
     githubLink: 'https://github.com/alexzhang1030',
     twitterLink: 'https://twitter.com/AlanBro23'
@@ -28,14 +28,14 @@ const teamMembers: ITeamMember[] = [
   {
     name: 'zgsgs',
     avatar: 'https://avatars.githubusercontent.com/u/22882957?v=4',
-    tag: 'Member',
+    tag: 'Core',
     address: 'Shanghai, China',
     githubLink: 'https://github.com/zgsgs'
   },
   {
     name: 'RainyNight9',
     avatar: 'https://avatars.githubusercontent.com/u/20129035?v=4',
-    tag: 'Member',
+    tag: 'Core',
     address: 'Beijing, China',
     link: 'https://RainyNight9.github.io',
     githubLink: 'https://github.com/RainyNight9'
@@ -43,13 +43,13 @@ const teamMembers: ITeamMember[] = [
   {
     name: 'Lo',
     avatar: 'https://avatars.githubusercontent.com/u/50254496?v=4',
-    tag: 'Member',
+    tag: 'Core',
     address: 'Wuxi, China',
     githubLink: 'https://github.com/LoTwT'
   },
   {
     name: 'chris-zhu',
-    tag: 'Member',
+    tag: 'Core',
     address: 'Chengdu, China',
     link: 'http://zyob.top',
     githubLink: 'https://github.com/chris-zhu'
