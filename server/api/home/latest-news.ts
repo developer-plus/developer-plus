db504e16d5f3277701a4daed7a9344c3ae0a7b4d
Hongbusi <q951686662@qq.com>
2022-06-13 09:53:22 +0800

    chore: weekly 5

--- server/api/home/latest-news.ts
+++ server/api/home/latest-news.ts
@@ -8,6 +8,11 @@ const withPrefix = (s: string) => `https://github.com/developer-plus/${s}`
 
 export default defineEventHandler<News[]>(() => [
   {
+    title: '周刊 - 第 5 期 ： iconfont 回来了。',
+    time: '2022-06-13',
+    link: withPrefix('weekly/blob/main/docs/issue-5.md')
+  },
+  {
     title: '周刊 - 第 4 期 ： developer-plus 公众号上线了。',
     time: '2022-06-06',
     link: withPrefix('weekly/blob/main/docs/issue-4.md')
