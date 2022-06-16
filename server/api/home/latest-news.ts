8f468f223fc5fa3c512f6371a04767a70b8fbae0
Hongbusi <q951686662@qq.com>
2022-06-16 16:07:51 +0800

    chore: update latest news

--- server/api/home/latest-news.ts
+++ server/api/home/latest-news.ts
@@ -21,25 +21,5 @@ export default defineEventHandler<News[]>(() => [
     title: '周刊 - 第 3 期 ： 六周年快乐。',
     time: '2022-05-30',
     link: withPrefix('weekly/blob/main/docs/issue-3.md')
-  },
-  {
-    title: 'developer-plus 官网上线了!',
-    link: withPrefix('developer-plus'),
-    time: '2022-05-23'
-  },
-  {
-    title: '周刊 - 第 2 期：developer-plus 官网上线了。',
-    link: withPrefix('weekly/blob/main/docs/issue-2.md'),
-    time: '2022-05-23'
-  },
-  {
-    title: 'mini-unocss 更名为 amcss，目前已进入开发阶段。',
-    link: withPrefix('amcss'),
-    time: '2022-05-21'
-  },
-  {
-    title: '周刊 - 第 1 期：梦开始的地方。',
-    link: withPrefix('weekly/blob/main/docs/issue-1.md'),
-    time: '2022-05-16'
   }
 ])
