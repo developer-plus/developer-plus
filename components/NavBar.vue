294de8e4d6698fc31fd720decd9abd13b161e0ee
likui628 <likui.it@gmail.com>
2022-06-23 19:03:04 +0800

    chore: navbar active icon

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -61,24 +61,26 @@ onUnmounted(() => {
     <div class="absolute top-25px left-50% translate-x--50% w-32px h-32px">
       <img src="/logo.png" alt="logo">
     </div>
-    <nuxt-link class="icon-primary" to="/" data-key="index" title="Home">
-      <div class="i-ri-home-smile-line" />
-    </nuxt-link>
-    <nuxt-link class="icon-primary" to="/plans" data-key="plans" title="Plans">
-      <div class="i-ri-todo-line" />
-    </nuxt-link>
-    <nuxt-link class="icon-primary" to="/weekly" data-key="weekly" title="Weekly">
-      <div class="i-ic-baseline-menu-book" />
-    </nuxt-link>
-    <!-- <nuxt-link class="icon-primary" to="/bookmarks" data-key="bookmarks" title="Bookmarks">
-    <div class="i-ic-round-bookmarks" />
-  </nuxt-link> -->
-    <nuxt-link class="icon-primary" to="/team-member" data-key="team-member" title="Team member">
-      <div class="i-ri-team-line" />
-    </nuxt-link>
-    <nuxt-link class="icon-primary" to="/sponsors" data-key="sponsors" title="Sponsors">
-      <div class="i-simple-icons-githubsponsors" />
-    </nuxt-link>
+    <nuxt-link
+      to="/" data-key="index" title="Home"
+      class="icon-primary i-ri-home-smile-line" active-class="!i-ri-home-smile-fill"
+    />
+    <nuxt-link
+      to="/plans" data-key="plans" title="Plans"
+      class="icon-primary i-ri-todo-line" active-class="!i-ri-todo-fill"
+    />
+    <nuxt-link
+      to="/weekly" data-key="weekly" title="Weekly"
+      class="icon-primary i-ri-calendar-line" active-class="!i-ri-calendar-fill"
+    />
+    <nuxt-link
+      to="/team-member" data-key="team-member" title="Team member"
+      class="icon-primary i-ri-team-line" active-class="!i-ri-team-fill"
+    />
+    <nuxt-link
+      to="/sponsors" data-key="sponsors" title="Sponsors"
+      class="icon-primary i-ri-heart-line" active-class="!i-ri-heart-fill"
+    />
     <div ref="boxRef" class="absolute top-2 right-0 translate-x-50% w-2 h-2 bg-light-900 rounded-full" />
   </div>
 </template>
