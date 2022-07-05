e4b77fa115b95f2bf1868a71018ca68b3cc783b9
Hongbusi <q951686662@qq.com>
2022-07-05 19:05:07 +0800

    chore: update latest news

--- server/api/home/latest-news.ts
+++ server/api/home/latest-news.ts
@@ -8,6 +8,11 @@ const withPrefix = (s: string) => `https://github.com/developer-plus/${s}`
 
 export default defineEventHandler<News[]>(() => [
   {
+    title: '周刊 - 第 8 期 ：Vue2.7 尝鲜，代号 “Naruto”。',
+    time: '2022-06-13',
+    link: withPrefix('weekly/blob/main/docs/weekly-8.md')
+  },
+  {
     title: '周刊 - 第 7 期 ： Facebook 标志的故事。',
     time: '2022-06-27',
     link: withPrefix('weekly/blob/main/docs/issue-7.md')
@@ -16,10 +21,5 @@ export default defineEventHandler<News[]>(() => [
     title: '周刊 - 第 6 期 ： 一个轻量的 canvas 库',
     time: '2022-06-20',
     link: withPrefix('weekly/blob/main/docs/issue-6.md')
-  },
-  {
-    title: '周刊 - 第 5 期 ： iconfont 回来了。',
-    time: '2022-06-13',
-    link: withPrefix('weekly/blob/main/docs/issue-5.md')
   }
 ])
