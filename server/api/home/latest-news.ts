15142a95093ef44cdadb53a0130e9f5081c776a0
洪布斯 <q951686662@qq.com>
2022-07-15 17:38:07 +0800

    chore: update latest news

--- server/api/home/latest-news.ts
+++ server/api/home/latest-news.ts
@@ -8,18 +8,18 @@ const withPrefix = (s: string) => `https://github.com/developer-plus/${s}`
 
 export default defineEventHandler<News[]>(() => [
   {
+    title: '周刊 - 第 9 期',
+    time: '2022-07-11',
+    link: withPrefix('weekly/blob/main/docs/weekly-9.md')
+  },
+  {
     title: '周刊 - 第 8 期 ：Vue2.7 尝鲜，代号 “Naruto”。',
-    time: '2022-06-13',
+    time: '2022-07-04',
     link: withPrefix('weekly/blob/main/docs/weekly-8.md')
   },
   {
     title: '周刊 - 第 7 期 ： Facebook 标志的故事。',
     time: '2022-06-27',
     link: withPrefix('weekly/blob/main/docs/issue-7.md')
-  },
-  {
-    title: '周刊 - 第 6 期 ： 一个轻量的 canvas 库',
-    time: '2022-06-20',
-    link: withPrefix('weekly/blob/main/docs/issue-6.md')
   }
 ])
